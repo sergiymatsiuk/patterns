41b7a1841195151cf28552c21b14f71d0e98b9e3
Sergiy Matsiuk <sergiymatsiuk1997@gmail.com>
2021-06-16 21:27:09 +0300

    singleton

---
+++ 01_singleton/singleton.ts
@@ -0,0 +1,18 @@
+class Matherland {
+
+    private static country : Matherland;
+
+    private constructor () {}
+
+    public static getCountry(): Matherland {
+        if (!Matherland.country) {
+            Matherland.country = new Matherland();
+        }
+        return Matherland.country;
+    }
+}
+
+const s1 = Matherland.getCountry();
+const s2 = Matherland.getCountry();
+
+console.log(s1===s2);
