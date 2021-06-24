ace6526ec7ffbcd6d3e0ee9f31dca87ccad9c910
Sergiy Matsiuk <sergiymatsiuk1997@gmail.com>
2021-06-24 22:34:36 +0300

    change 04/builder

--- Creational/04_builder/builder.js
+++ Creational/04_builder/builder.js
@@ -1,64 +1,25 @@
-var ConcreteCofe1 = /** @class */ (function () {
-    function ConcreteCofe1() {
-        this.reset();
+var CarRental = /** @class */ (function () {
+    function CarRental(brand, model) {
+        this.brand = brand;
+        this.model = model;
+        this.airConditioning = false;
     }
-    ConcreteCofe1.prototype.reset = function () {
-        this.product = new Cofe1();
+    CarRental.prototype.addAirConditioning = function () {
+        this.airConditioning = true;
+        return this;
     };
-    ConcreteCofe1.prototype.cofeWithMilk = function () {
-        this.product.parts.push('with milk');
+    CarRental.prototype.addNumberOfDays = function (numberOfDays) {
+        this.numberOfDays = numberOfDays;
+        return this;
     };
-    ConcreteCofe1.prototype.cofeWithSuger = function () {
-        this.product.parts.push('with sugar');
+    CarRental.prototype.setColor = function (color) {
+        this.color = color;
+        return this;
     };
-    ConcreteCofe1.prototype.cofeWithChocolate = function () {
-        this.product.parts.push('with chocolate');
+    CarRental.prototype.order = function () {
+        return this;
     };
-    ConcreteCofe1.prototype.getProduct = function () {
-        var result = this.product;
-        this.reset();
-        return result;
-    };
-    return ConcreteCofe1;
+    return CarRental;
 }());
-var Cofe1 = /** @class */ (function () {
-    function Cofe1() {
-        this.parts = [];
-    }
-    Cofe1.prototype.listParts = function () {
-        console.log("Cofe " + this.parts.join(', ') + "\n");
-    };
-    return Cofe1;
-}());
-var Director = /** @class */ (function () {
-    function Director() {
-    }
-    Director.prototype.setBuilder = function (builder) {
-        this.builder = builder;
-    };
-    Director.prototype.buildMinimalProduct = function () {
-        this.builder.cofeWithMilk();
-    };
-    Director.prototype.buildFullProduct = function () {
-        this.builder.cofeWithMilk();
-        this.builder.cofeWithSuger();
-        this.builder.cofeWithChocolate();
-    };
-    return Director;
-}());
-function clientCode(director) {
-    var builder = new ConcreteCofe1();
-    director.setBuilder(builder);
-    console.log('Standard basic cofe:');
-    director.buildMinimalProduct();
-    builder.getProduct().listParts();
-    console.log('Standard full cofe:');
-    director.buildFullProduct();
-    builder.getProduct().listParts();
-    console.log('Custom product:');
-    builder.cofeWithMilk();
-    builder.cofeWithChocolate();
-    builder.getProduct().listParts();
-}
-var director = new Director();
-clientCode(director);
+var car = new CarRental('bmw', 'x5').addAirConditioning().addNumberOfDays(2).setColor('black').order();
+console.log(car);
--- Creational/04_builder/builder.ts
+++ Creational/04_builder/builder.ts
@@ -1,92 +1,29 @@
-interface Cofe {
-    cofeWithMilk(): void;
-    cofeWithSuger(): void;
-    cofeWithChocolate(): void;
+
+  
+class CarRental {
+    airConditioning: boolean = false
+    numberOfDays: number
+    color: string
+
+    constructor(public brand: string, public model: string) {}
+
+    addAirConditioning(): CarRental {
+        this.airConditioning = true
+        return this
+    }
+
+    addNumberOfDays(numberOfDays: number): CarRental {
+        this.numberOfDays = numberOfDays
+        return this
+    }
+
+    setColor(color: string): CarRental {
+        this.color = color
+        return this
+    }
+
 }
 
-class ConcreteCofe1 implements Cofe {
+const car = new CarRental('bmw', 'x5').addAirConditioning().addNumberOfDays(2).setColor('black');
 
-    private product: Cofe1;
-
-    constructor() {
-        this.reset();
-    }
-
-    public reset(): void {
-        this.product = new Cofe1();
-    }
-
-    public cofeWithMilk(): void {
-        this.product.parts.push('with milk');
-    }
-
-    public cofeWithSuger(): void {
-        this.product.parts.push('with sugar');
-    }
-
-    public cofeWithChocolate(): void {
-        this.product.parts.push('with chocolate');
-    }
-
-    public getProduct(): Cofe1 {
-        const result = this.product;
-        this.reset();
-        return result;
-    }
-}
-
-class Cofe1 {
-    public parts: string[] = [];
-
-    public listParts(): void {
-        console.log(`Cofe ${this.parts.join(', ')}\n`);
-    }
-}
-
-class Director {
-    private builder: Cofe;
-
-    public setBuilder(builder:Cofe): void {
-        this.builder = builder;
-    }
-
-    public buildMinimalProduct(): void {
-        this.builder.cofeWithMilk();
-    }
-
-    public buildFullProduct(): void {
-        this.builder.cofeWithMilk();
-        this.builder.cofeWithSuger();
-        this.builder.cofeWithChocolate();
-    }
-}
-
-function clientCode (director: Director) {
-    const builder = new ConcreteCofe1();
-    director.setBuilder(builder);
-
-    console.log('Standard basic cofe:');
-    director.buildMinimalProduct();
-    builder.getProduct().listParts();
-
-    console.log('Standard full cofe:');
-    director.buildFullProduct();
-    builder.getProduct().listParts();
-
-    console.log('Custom product:');
-    builder.cofeWithMilk();
-    builder.cofeWithChocolate();
-    builder.getProduct().listParts();
-}
-
-const director = new Director();
-clientCode(director);
-
-// Standard basic cofe:
-// Cofe with milk
-
-// Standard full cofe:
-// Cofe with milk, with sugar, with chocolate
-
-// Custom product:
-// Cofe with milk, with chocolate
+console.log(car)
